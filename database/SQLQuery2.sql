1589e2f5a6c81319b04afc8fd7a6a5903ddf06b1
Justin <justinmanoj12@gmail.com>
2024-06-30 00:43:57 -0400

    Updated DB

--- database/SQLQuery2.sql
+++ database/SQLQuery2.sql
@@ -1,1 +1,18 @@
 CREATE DATABASE OnlineShopping; 
+
+
+
+CREATE TABLE Customer (
+	CustomerID int NOT NULL PRIMARY KEY, 
+	firstName varchar(50), 
+	lastName varchar(50), 
+	phoneNumber VARCHAR(15), 
+	address VARCHAR(50), 
+	city VARCHAR(50), 
+	state VARCHAR(50), 
+	zipCode VARCHAR(10), 
+	country VARCHAR(50), 
+	password VARCHAR(50) NOT NULL
+
+);
+
